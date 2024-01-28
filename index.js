df1311d0727b7a17d1ed6c466982377a86ec3d65
at-atanasov <at.atanasov@gmail.com>
2024-01-28 19:17:58 +0200

    parsing variables from url

--- index.js
+++ index.js
@@ -67,11 +67,14 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"); //__di
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathname = req.url;
+
+  //console.log(url.parse(req.url, true)) // с parse превръщаме url-а в обект и можем да вземем променливите. С true парсваме и query string-а
 
   //Overview page
 
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -83,11 +86,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product page
-  } else if (pathName === "/product") {
-    res.end("This is the product!");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
 
     // Api
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
