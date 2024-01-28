2662d5f8d3fc751e36b22268a9facffd929a0f92
at-atanasov <at.atanasov@gmail.com>
2024-01-28 19:29:24 +0200

    exporting and importing own modules, npm init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs"); //require file system in order to be able to work with files
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate"); //own modules are usually included after the core modules
 
 //Node.js Built-in Modules https://www.w3schools.com/nodejs/ref_modules.asp
 
@@ -35,21 +36,6 @@ const url = require("url");
 
 ////////////////////////////////////
 //SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // /{%PRODUCTNAME%}/g наклонките отбелязват regular expression, а g, че е глобал и се отнася за всички пъти в които се срещне
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
