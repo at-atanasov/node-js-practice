2c0c82219c414e17e587540d4705135f130f8c75
at-atanasov <at.atanasov@gmail.com>
2024-01-29 22:20:55 +0200

    third party modules, prettier

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const fs = require("fs"); //require file system in order to be able to work with files
-const http = require("http");
-const url = require("url");
-const replaceTemplate = require("./modules/replaceTemplate"); //own modules are usually included after the core modules
+const fs = require('fs'); //require file system in order to be able to work with files
+const http = require('http');
+const url = require('url');
+
+const slugify = require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate'); //own modules are usually included after the core modules
 
 //Node.js Built-in Modules https://www.w3schools.com/nodejs/ref_modules.asp
 
@@ -39,18 +42,20 @@ const replaceTemplate = require("./modules/replaceTemplate"); //own modules are 
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"); //__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); //__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
@@ -60,40 +65,40 @@ const server = http.createServer((req, res) => {
 
   //Overview page
 
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
-      .join("");
+      .join('');
 
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
     res.end(output);
 
     // Product page
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
 
     // Api
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
     // Not found
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "My-header": "test header",
+      'Content-type': 'text/html',
+      'My-header': 'test header',
     });
-    res.end("<h1>This page could not be found!</h1>");
+    res.end('<h1>This page could not be found!</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000!");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000!');
 });
